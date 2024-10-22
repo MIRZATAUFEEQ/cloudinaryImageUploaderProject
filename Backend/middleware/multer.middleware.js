b6028841b7ff807013b7438fefd7400140922ea5
Mirza <mirzataufeeq98436@gmail.com>
2024-10-22 13:36:52 +0530

    update

--- Backend/middleware/multer.middleware.js
+++ Backend/middleware/multer.middleware.js
@@ -1,6 +1,13 @@
 import multer from 'multer';
 import path from 'path';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Define __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // Configure multer storage😂🥰😎😐😃✅
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
